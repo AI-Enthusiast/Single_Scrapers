9a844d4b53e3b271a546d7aa3382dc4c1559b243
cormac <cormacdacker@gmail.com>
2024-12-05 09:52:07 -0800

    Updated functions

--- podcasts/podscripts_scraper.py
+++ podcasts/podscripts_scraper.py
@@ -19,62 +19,66 @@ def get_driver():  # set up the driver
     time.sleep(get_wait_time())
     return driver
 
-if not Favs_only:
-    pods = ['lions-led-by-donkeys-podcast',
-            'behind-the-bastards',
-            'stuff-you-should-know',
-            'my-favorite-murder-with-karen-kilgariff-and-georgia-hardstark',
-            'last-podcast-on-the-left',
-            'the-dollop-with-dave-anthony-and-gareth-reynolds',
-            'crime-junkie']
-else:
-    favs = ['lions-led-by-donkeys-podcast',
-            'behind-the-bastards']
-    pods = favs
+def get_podcasts():
 
-def build_url(pod_name, page):
-    url_prefix = "https://podscripts.co/podcasts/"
-    url_suffix = "?page="
-    return url_prefix + pod_name + url_suffix + str(page) if page > 1 else url_prefix + pod_name
+    if not Favs_only:
+        pods = ['lions-led-by-donkeys-podcast',
+                'behind-the-bastards',
+                'stuff-you-should-know',
+                'my-favorite-murder-with-karen-kilgariff-and-georgia-hardstark',
+                'last-podcast-on-the-left',
+                'the-dollop-with-dave-anthony-and-gareth-reynolds',
+                'crime-junkie']
+    else:
+        favs = ['lions-led-by-donkeys-podcast',
+                'behind-the-bastards']
+        pods = favs
+
+    def build_url(pod_name, page):
+        url_prefix = "https://podscripts.co/podcasts/"
+        url_suffix = "?page="
+        return url_prefix + pod_name + url_suffix + str(page) if page > 1 else url_prefix + pod_name
 
 
-driver = get_driver()
-output = []  # podcast, title, link
-old_len = 0
-for podcast in pods:
-    pod_pretty = podcast.replace('-', ' ').title()
-    more_pages = True
-    page = 1
-    len_change = 0
-    while more_pages:
-        try:
-            driver.get(build_url(podcast, page))
-            time.sleep(get_wait_time())
-            items = driver.find_elements("xpath",
-                                         "//div[@class='list-main-wrap fl-wrap card-listing']/div[@class='listing-item']/article[@class='geodir-category-listing fl-wrap']")
+    driver = get_driver()
+    output = []  # podcast, title, link
+    old_len = 0
+    for podcast in pods:
+        pod_pretty = podcast.replace('-', ' ').title()
+        more_pages = True
+        page = 1
+        len_change = 0
+        while more_pages:
+            try:
+                driver.get(build_url(podcast, page))
+                time.sleep(get_wait_time())
+                items = driver.find_elements("xpath",
+                                             "//div[@class='list-main-wrap fl-wrap card-listing']/div[@class='listing-item']/article[@class='geodir-category-listing fl-wrap']")
 
-        except:
-            more_pages = False
-            break
-        for item in items:
-            link = item.find_element("xpath", ".//a").get_attribute("href")
-            title = item.find_element("xpath", ".//h3/a").text
-            output.append([pod_pretty, title, link])
-        if len_change == len(output):
-            more_pages = False
-        len_change = len(output)
+            except:
+                more_pages = False
+                break
+            for item in items:
+                link = item.find_element("xpath", ".//a").get_attribute("href")
+                title = item.find_element("xpath", ".//h3/a").text
+                output.append([pod_pretty, title, link])
+            if len_change == len(output):
+                more_pages = False
+            len_change = len(output)
 
-        page += 1
+            page += 1
 
-    print(f"Total episodes for {pod_pretty}: {len(output) - old_len}")
-    old_len = len(output)
+        print(f"Total episodes for {pod_pretty}: {len(output) - old_len}")
+        old_len = len(output)
 
-# drop all instances were title contains "PREVIEW"
-output = [x for x in output if "PREVIEW" not in x[1]]
+    # drop all instances were title contains "PREVIEW"
+    output = [x for x in output if "PREVIEW" not in x[1]]
 
-if not Favs_only:
-    pd.DataFrame(output, columns=["podcast", "title", "link"]).to_csv("podscripts.csv", index=False)
-else:
-    pd.DataFrame(output, columns=["podcast", "title", "link"]).to_csv("podscripts_favs.csv", index=False)
-driver.close()
+    # if not Favs_only:
+    #     pd.DataFrame(output, columns=["podcast", "title", "link"]).to_csv("podscripts.csv", index=False)
+    # else:
+    #     pd.DataFrame(output, columns=["podcast", "title", "link"]).to_csv("podscripts_favs.csv", index=False)
+    driver.close()
+    driver.quit()
+    return pd.DataFrame(output, columns=["podcast", "title", "link"])
 
