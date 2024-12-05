94e2ed7a9e36b03782d8a4bf0c1fca42e41bb38b
cormac <cormacdacker@gmail.com>
2024-12-05 09:52:20 -0800

    Updated Main

--- podcasts/parrallel.py
+++ podcasts/parrallel.py
@@ -1,5 +1,6 @@
 import concurrent
 import os
+# print the current path
 import time
 import tqdm
 import random
@@ -10,10 +11,12 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from concurrent.futures import ThreadPoolExecutor
 from threading import Semaphore
-thread_num = 8
+
+thread_num = 1
+Favs_only = True
 def get_wait_time(quick=True):
     if quick:
-        return random.uniform(4, 9)
+        return random.uniform(3, 4)
     else:
         return random.uniform(30, 60)
 
@@ -27,10 +30,9 @@ def download_mp3(driver, podcast, title, url, semaphore):
     # first check if the podcast folder exists and the mp3 file is not already downloaded
     if os.path.exists(f"/podcasts/downloads/{podcast}/{title}.mp3"):
         return
-
     with semaphore:
         # try:
-        os.chdir("/podcasts/downloads")
+        # os.chdir("podcasts/downloads")
         if not os.path.exists(podcast):
             os.makedirs(podcast)
         os.chdir(podcast)
@@ -48,7 +50,8 @@ def download_mp3(driver, podcast, title, url, semaphore):
         # //*[@id="main_pod_player_parent"]
         # //*[@id="main_pod_player"]
         # /html/body/div/div[3]/div/section/div/div[3]/div/div/div/div[3]/audio/source
-        mp3 = driver.find_element("xpath", "/html/body/div/div[3]/div/section/div/div[3]/div/div/div/div[3]/audio/source")
+        # /html/body/div[1]/div[3]/div/section/div/div[4]/div/div/div/div[3]/audio/source
+        mp3 = driver.find_element("xpath", "/html/body/div[1]/div[3]/div/section/div/div[4]/div/div/div/div[3]/audio/source")
         mp3_url = mp3.get_attribute("src")
         bad_chars = ["<", ">", ":", "\"", "/", "\\", "|", "?", "*"]
         for char in bad_chars:
@@ -59,8 +62,13 @@ def download_mp3(driver, podcast, title, url, semaphore):
     # except Exception as e:
     #     print(f"Error downloading {title}: {e}")
 
-def main():
-    pods = pd.read_csv("podscripts.csv")
+def scraper(pods = None):
+    if pods is None:
+        if not Favs_only:
+            pods = pd.read_csv("podscripts.csv")
+        else:
+            pods = pd.read_csv("podscripts_favs.csv")
+    print(f'Starting download of {len(pods)} podcasts')
     semaphore = Semaphore(thread_num)  # Limit to x number of threads
     drivers = [get_driver() for _ in range(thread_num)]
     with ThreadPoolExecutor(max_workers=thread_num) as executor:
@@ -77,4 +85,4 @@ def main():
         driver.quit()
 
 if __name__ == "__main__":
-    main()
+    scraper()
