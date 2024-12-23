a463012127432e58878886532a5824ade02e702b
Cormac <cormacdacker@gmail.com>
2024-12-23 13:22:54 -0800

    Pass the driver

--- Media/podcasts/__init__.py
+++ Media/podcasts/__init__.py
@@ -2,6 +2,6 @@ from .podscripts_scraper import get_podcasts
 from .parrallel import scraper
 
 def podcast_update(podcasts_path, test_run):
-    pod_list = get_podcasts(podcasts_path, test_run)
-    scraper(pod_list)
+    pod_list, driver = get_podcasts(podcasts_path, test_run) # todo fix pathing bug
+    scraper(pod_list, driver)
     print("Podcasts updated.")
